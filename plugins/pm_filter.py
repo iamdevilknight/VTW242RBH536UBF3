3b0e748d43b7ffb2cfeee5568dcf560388e22691
iamdevilknight <66716711+iamdevilknight@users.noreply.github.com>
2022-07-08 21:21:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -110,13 +110,13 @@ async def next_page(bot, query: CallbackQuery):
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton(f"🗓 {round(int(offset) / 5) + 1} / {round(total / 5)}", callback_data="pages"),
+            [InlineKeyboardButton(f"🗓 {round(int(offset / 5)) + 1} / {round(total / 5)}", callback_data="pages"),
              InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
                 InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"🗓 {round(int(offset) / 5) + 1} / {round(total / 5)}", callback_data="pages"),
+                InlineKeyboardButton(f"🗓 {round(int(offset / 5)) + 1} / {round(total / 5)}", callback_data="pages"),
                 InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
